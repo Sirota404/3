af97e69c125e582d5a01d7894c1fe9b8b8b643c2
Sirota404 <153153025+Sirota404@users.noreply.github.com>
2024-02-19 10:34:25 +0800

    Update Program.cs

--- 3/3/Program.cs
+++ 3/3/Program.cs
@@ -1,133 +1,125 @@
 using System;
-using System.Linq.Expressions;
 
-class QuadraticEquationSolver
+class Program
 {
-    static void Main()
-    {
-        while (true)
-            try
-            {
-                Console.WriteLine("a * x^2 + b * x + c = 0");
-                Console.WriteLine("Введите значение a:");
-                int a = ParseInput();
-
-                Console.WriteLine("Введите значение b:");
-                int b = ParseInput();
-
-                Console.WriteLine("Введите значение c:");
-                int c = ParseInput();
-
-                double[] solutions = QuadraticEquation(a, b, c);
-
-
-                if (solutions.Length == 2)
-                {
-                    Console.WriteLine($"x1 = {solutions[0]}, x2 = {solutions[1]}");
-                }
-                else if (solutions.Length == 1)
-                {
-                    Console.WriteLine($"x = {solutions[0]}");
-                }
-
-                else
-                {
-                    throw new NoException();
-                }
-                Console.WriteLine("Введите 'да' чтобы запустить заново:");
-                Console.ResetColor();
-
-                string input = Console.ReadLine();
-
-                if (input.ToLower() == "да")
-                {
-                    Console.WriteLine("Вы ввели 'да'. \n");
-                    break;
-                }
-                else if (input.ToLower() == "нет")
-                {
-                    Console.WriteLine("Вы ввели 'нет'.\n");
-                    Environment.Exit(0);
-                }
-                else
-                {
-                    Console.WriteLine("Вы ввели неверное значение.");
-                    Environment.Exit(0);
-                }
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Неверный формат данных");
-                Main();
-            }
-            catch (NoException ex)
-            {
-                FormatData(ex, Severity.Warning);
-                Main();
-            }
-
-
-    }
-
-
-    static int ParseInput()
-    {
-        int value;
-
-        if (!int.TryParse(Console.ReadLine(), out value))
-        {
-            throw new FormatException();
-        }
-
-        return value;
-    }
-
-    static double[] QuadraticEquation(int a, int b, int c)
-    {
-        double discriminant = b * b - 4 * a * c;
-
-        if (discriminant < 0)
-        {
-            throw new NoException();
-        }
-        else if (discriminant == 0)
-        {
-            return new double[] { -b / (2.0 * a) };
-        }
-        else
-        {
-            double sqrtDiscriminant = Math.Sqrt(discriminant);
-            return new double[] { (-b + sqrtDiscriminant) / (2.0 * a), (-b - sqrtDiscriminant) / (2.0 * a) };
-        }
-    }
-
-    static void FormatData(Exception ex, Severity severity = Severity.Error)
-    {
-        Console.ForegroundColor = ConsoleColor.Black;
-        switch (severity)
-        {
-            case Severity.Warning:
-                Console.BackgroundColor = ConsoleColor.Yellow;
-                break;
-            case Severity.Error:
-                Console.BackgroundColor = ConsoleColor.Red;
-                break;
-        }
-
-        Console.WriteLine(ex.Message);
-        Console.ResetColor();
-    }
-
+static void Main()
+{
+StartInput();
 }
 
-
-class NoException : Exception
+private static void DisplaySolutions(int a, int b, int c, int x1, int x2, int D)
 {
-    public NoException() : base("Вещественных значений не найдено") { }
+Console.WriteLine("a * x^2 + b * x + c = 0");
+Console.WriteLine($"a = {a}");
+Console.WriteLine($"b = {b}");
+Console.WriteLine($"c = {c}");
+
+if (D >= 0)
+{
+Console.WriteLine("Решения:");
+Console.WriteLine($"x1 = {x1}");
+Console.WriteLine($"x2 = {x2}");
+}
+else
+{
+Console.WriteLine("Вещественных значений не найдено");
 }
 
-enum Severity
+Console.ReadLine();
+}
+
+private static void StartInput()
 {
-    Warning,
-    Error
+Console.WriteLine("a * x^2 + b * x + c = 0");
+Console.WriteLine("Введите значение a:");
+var a = Console.ReadLine();
+Console.WriteLine("Введите значение b:");
+var b = Console.ReadLine();
+Console.WriteLine("Введите значение c:");
+var c = Console.ReadLine();
+
+var processing = Processing(a, b, c);
+var aInt = processing.a;
+var bInt = processing.b;
+var cInt = processing.c;
+
+try
+{
+var solution = Solution(aInt, bInt, cInt);
+DisplaySolutions(aInt, bInt, cInt, solution.x1, solution.x2, solution.D);
+}
+catch (Exception ex)
+{
+Console.WriteLine(ex.Message);
+Console.ReadLine();
+}
+}
+
+private static (int a, int b, int c) Processing(string a, string b, string c)
+{
+var i = 0;
+string perem = "a";
+
+try
+{
+int aInt;
+int bInt;
+int cInt;
+aInt = Int32.Parse(a.Trim('a'));
+i++;
+bInt = Int32.Parse(b.Trim('b'));
+i++;
+cInt = Int32.Parse(c.Trim('c'));
+return (aInt, bInt, cInt);
+}
+catch (System.OverflowException)
+{
+Console.BackgroundColor = ConsoleColor.Green;
+Console.WriteLine("Диапазон значений int = От -2 147 483 648 до 2 147 483 647");
+Console.ResetColor();
+Console.ReadLine();
+Console.Clear();
+return (0, 0, 0);
+}
+catch
+{
+if (i == 0) { perem = "a"; } else if (i == 1) { perem = "b"; } else if (i == 2) { perem = "c"; }
+
+Console.BackgroundColor = ConsoleColor.Red;
+Console.WriteLine($"Неверный формат параметра {perem}");
+Console.WriteLine($" a = {a} \n b = {b} \n c = {c}");
+Console.ResetColor();
+Console.ReadLine();
+Console.Clear();
+return (0, 0, 0);
+//return StartInput();
+}
+}
+
+private static (int x1, int x2, int D) Solution(int a, int b, int c)
+{
+int discriminant;
+int x1;
+int x2;
+
+discriminant = (int)(Math.Pow(b, 2) - 4 * a * c);
+
+try
+{
+if (discriminant > 0 || discriminant == 0)
+{
+x1 = (int)((-b + Math.Sqrt(discriminant)) / (2 * a));
+x2 = (int)((-b - Math.Sqrt(discriminant)) / (2 * a));
+return (x1, x2, discriminant);
+}
+else
+{
+throw new NotImplementedException("Вещественных значений не найдено");
+}
+}
+catch
+{
+throw;
+}
+}
 }
